9031ba713cc90bd764d2b2a95e7db687837d0f73
mbilalakmal <you@example.com>
2019-10-19 03:48:37 +0500

    Removed Projector And Computer Bools

--- Course.hpp
+++ Course.hpp
@@ -2,6 +2,7 @@
 #define COURSE
 
 #include<string>
+#include<vector>
 using namespace std;
 
 //describes a course which can be core/elective & theory/lab
@@ -14,14 +15,21 @@ class Course{
 				int		duration,
 				string&	department,
 				string&	school,
-				bool	isCoreCourse)
+				bool	isCoreCourse,
+				bool	isLabCourse,
+				bool	needsComputerLab,
+				bool	needsElectricalLab
+				)
 			:	id(id),
 				courseCode(courseCode),
 				title(title),
 				duration(duration),
 				department(department),
 				school(school),
-				isCoreCourse(isCoreCourse){}
+				isCoreCourse(isCoreCourse),
+				isLabCourse(isLabCourse),
+				needsComputerLab(needsComputerLab),
+				needsElectricalLab(needsElectricalLab){}
 		
 		int getId() const {return id;}
 
@@ -44,9 +52,8 @@ class Course{
 		bool getIsLabCourse() const { return isLabCourse;}
 
 		bool getNeedsComputerLab() const {return needsComputerLab;}
+
 		bool getNeedsElectricalLab() const {return needsElectricalLab;}
-		bool getNeedsProjector() const {return needsProjector;}
-		bool getNeedsComputer() const {return needsComputer;}
 		
 	private:
 		
@@ -56,20 +63,15 @@ class Course{
 		string		school;				//CS OR EE OR MG OR MT OR SS
 		string		title;				//Computer Architecture
 		string		shortTitle;			//CA, PROB, OS-LAB
-		int			duration;		//1, 3, 4
+		int			duration;			//1, 3, 4
 		vector<int>	offeredToBatches;	//2017 & 2018
 		bool		isCoreCourse;		//true = is a core course
-		bool		isLabCourse;		//true = duration needs to
-										//be continous and lab constraints apply
+		bool		isLabCourse;		//true = is a lab course
 
-		//facilities
-		/*
-		//need to add more like language lab and EE & BBA stuff
-		*/
-		bool		needsComputerLab;	//true = is a computer lab
-		bool		needsElectricalLab;	//true = is an electrical lab
-		bool		needsProjector;		//true = has a working projector
-		bool		needsComputer;		//true = has a viper in class
+		bool		needsComputerLab;	//true = needs a computer lab
+		bool		needsElectricalLab;	//true = needs an electrical lab
+		// bool		needsProjector;		//Not needed
+		// bool		needsComputer;		//Moved to teacher
 	
 };
 
