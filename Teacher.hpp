57c7dc1af89f185080f80609b41931f0f3d041f2
mbilalakmal <you@example.com>
2019-10-19 04:19:11 +0500

    Added Senior And Preferred Slots For Teachers

--- Teacher.hpp
+++ Teacher.hpp
@@ -3,6 +3,7 @@
 
 #include<string>
 #include<list>
+#include<vector>
 using namespace std;
 
 class CourseClass;
@@ -12,10 +13,27 @@ class Teacher{
 	
 	public:
 		
-		Teacher(int id,
-				string& name)
-			: 	id(id),
-				name(name){}
+		Teacher(	int		id,
+					string& name,
+					string&	department,
+					bool	isSenior,
+					bool	needsComputer,
+					string	building,
+					int		floor,
+					vector<bool>	preferredSlots)
+				: 	id(id),
+					name(name),
+					department(department),
+					isSenior(isSenior),
+					needsComputer(needsComputer),
+					building(building),
+					floor(floor),
+					preferredSlots(preferredSlots){}
+
+		~Teacher(){
+			//does not delete the objects
+			courseClasses.clear();
+		}
 
 		void addCourseClass(CourseClass* courseClass){
 			courseClasses.push_back(courseClass);
@@ -27,16 +45,44 @@ class Teacher{
 		
 		const string& getDepartment() const {return department;}
 
+		bool getIsSenior() const {return isSenior;}
+
+		bool getNeedsComputer() const {return needsComputer;}
+
+		const string& getBuilding() const {return building;}
+
+		int getFloor() const {return floor;}
+
+		const vector<bool>& getPreferredSlots() const
+			{return preferredSlots;}
+
 		const list<CourseClass*>& getCourseClasses() const
 			{return courseClasses;}
 	
 	private:
 
-	int					id;				//unique identifier for the teacher
-	string				name;			//full name of the teacher
-	string				department;		//dept of the teacher
-	
-	list<CourseClass*>	courseClasses;	//courseClasses taught by the teacher
+	int		id;				//unique identifier for the teacher
+	string	name;			//full name of the teacher
+	string	department;		//dept of the teacher
+
+	bool	isSenior;		//true = teacher is a doctor (phd) senior
+	bool	needsComputer;	//true = teacher doesn't carry laptop
+
+	/*
+	Teacher's location
+	*/
+	string	building;		//CS, EE
+	int		floor;			//0, 1, 2(in EE)
+
+	/*
+	Teacher's preferred time slots
+	These are specific to each individual
+	[weekDays * totalHours]
+	*/
+	vector<bool>		preferredSlots;
+
+	//course classes taught by the teacher
+	list<CourseClass*>	courseClasses;
 };
 
 #endif
