08261adba8a3cf8b1afa60a8a4a22f30238881a4
mbilalakmal <you@example.com>
2019-10-21 21:10:51 +0500

    Multiple (And 0) teachers allowed for courseClasses

--- CourseClass.hpp
+++ CourseClass.hpp
@@ -13,16 +13,10 @@ class CourseClass{
 	
 	public:
 		
-		CourseClass(	Teacher*	teacher,
-						Course*		course)
+		CourseClass(	Course*		course)
 					:	id(nextId++),
-						teacher(teacher),
 						course(course),
-						strength(0){
-				
-			//add this course class to the given teacher's assigned CCs
-			teacher->addCourseClass(this);
-		}
+						strength(0){}
 		
 		~CourseClass(){}
 
@@ -34,6 +28,13 @@ class CourseClass{
 
 			//add the section's strength to the course class
 			strength += section->getStrength();
+		}
+
+		//add this course class to the given teacher &
+		//add the teacher to this course class' teachers
+		void addTeacher(Teacher* teacher){
+			teacher->addCourseClass(this);
+			teachers.push_back(teacher);
 		}
 
 		//returns whether another course class has a common section
@@ -48,13 +49,24 @@ class CourseClass{
 					}
 				}
 			}
-			//No common sections
+			//No common section
 			return false;
 		}
 
-		//returns whether another course class has the same teacher
-		bool teacherOverlaps(const CourseClass& courseClass ) const {
-			return teacher->getId() == courseClass.teacher->getId();
+		//returns whether another course class has a common teacher
+		bool teachersOverlap(const CourseClass& courseClass ) const {
+
+			for(auto it = teachers.begin(); it != teachers.end(); it++){
+				for(auto it_c = courseClass.getTeachers().begin();
+					it_c != courseClass.getTeachers().end(); it_c++){
+					//If both course classes contain a teacher (ID)
+					if( ( *it )->getId() == ( *it_c )->getId() ){
+						return true;
+					}
+				}
+			}
+			//No common teacher
+			return false;
 		}
 
 		int getId() const {return id;}
@@ -63,7 +75,8 @@ class CourseClass{
 		
 		const Course& getCourse() const {return *course;}
 
-		const Teacher& getTeacher() const {return *teacher;}
+		const list<Teacher*>& getTeachers() const
+			{return teachers;}
 
 		const list<StudentSection*>& getSections() const
 			{return sections;}
@@ -78,9 +91,11 @@ class CourseClass{
 		int			id;			//unique & auto-inc
 		int			strength;	//sum of strengths of sections
 		Course*		course;		//course reference
-		Teacher*	teacher;	//teacher reference
+		
+		//teacher(s) teaching this course class (multiple for freshies' labs)
+		list<Teacher*> teachers;
 
-		//parent sections included in this course class
+		//atomic sections included in this course class
 		list<StudentSection*> sections;
 };
 
