955ed6ec2c47ef232c3b9d8e3b7a52fc5e80a0a7
mbilalakmal <you@example.com>
2019-11-17 21:07:56 +0500

    Helper functions in constraints checking

---
+++ ConstraintsHelper.hpp
@@ -0,0 +1,88 @@
+#ifndef C_HELPER
+#define C_HELPER
+
+#include<vector>
+#include<unordered_set>
+using namespace std;
+
+/*
+HELPER FUNCTIONS FOR CHECK CONSTRAINTS
+*/
+
+//returns length of the longest consecutive subsequence
+int longestConsecutive(const vector<int>& dailySlots){
+    unordered_set<int> dailySlotsSet;
+    int result = 0;
+
+    //insert into set
+    for(const auto& slot: dailySlots){
+        dailySlotsSet.insert(slot);
+    }
+  
+    /*
+    check each subsequence & update max
+    */
+    for(const auto& slot: dailySlots){
+        //check if previous slot exists otherwise this is the start
+        if( dailySlotsSet.find( slot - 1 ) == dailySlotsSet.end() ){
+            //check for next slots in sequence
+            int j = slot;
+            while ( dailySlotsSet.find(j) != dailySlotsSet.end() ){
+                j++;
+            }
+            //update max
+            result = max( result, j - slot );
+           
+        }
+    }
+
+    return result;
+}
+
+int maxConsecutive(const vector< vector<int> >& slotsByDay){
+    int max = 0;
+    for(const auto& slots: slotsByDay){
+        int consecutive = longestConsecutive( slots );
+        if( consecutive > max ){
+            max = consecutive;
+        }
+    }
+    return max;
+}
+
+int maxDaily(const vector< vector<int> >& slotsByDay){
+    int max = 0;
+    for(const auto& slots: slotsByDay){
+        int daily = slots.size();
+        if( daily > max ){
+            max = daily;
+        }
+    }
+    return max;
+}
+
+int weekDays(const vector< vector<int> >& slotsByDay){
+    int days = 0;
+    for(const auto& slots: slotsByDay){
+        if( slots.size() ){
+            days++;
+        }
+    }
+    return days;
+}
+
+bool oneAtATime(const vector< vector<int> >& slotsByDay){
+    
+    for(const auto& slots: slotsByDay){
+        unordered_set<int> slotsSet;
+        for(const auto& slot: slots){
+            bool inserted = slotsSet.insert( slot ).second;
+            if( !inserted ){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+#endif
