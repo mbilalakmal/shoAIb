dd94f9823d869f38bc2c55d8b982f955a13c8a17
mbilalakmal <you@example.com>
2019-12-01 02:05:18 +0500

    Teacher after new convention

--- Teacher.hpp
+++ Teacher.hpp
@@ -1,90 +1,61 @@
 #ifndef TEACHER
 #define TEACHER
 
-#include<vector>
-using namespace std;
+#include <string>           //string
+#include <vector>           //vector
+#include <unordered_map>    //unordered_map
+#include "json.hpp"         //nlohmann::json
 
-class Lecture;
-
-//maximum lectures a teacher can teach consecutively
-#define MAX_T_CONT_LECT 3
-
-//maximum lectures a teacher can teach on a day
-#define MAX_T_DAILY_LECT 4
-
-//maximum days a teacher has to show up
-#define MAX_T_WEEKDAYS 4
-
-#include"json.hpp"
-using nlohmann::json;
+typedef std::vector<std::vector<bool>> AVAILABOOL;
 
 /*
-Describes a teacher with 5 attributes including assigned
-lectures and available slots according to requirements & policies
+Describes a teacher
 
 CONSTRAINTS:
-1. Day, Room, & Time assigned can be specified
-2. Teacher must not have more than 2 consecutive classes
-3. Teacher must have a day off
+1. Day, Room, & Time availability can be specified
+2. Teacher must not have more than 3 consecutive classes
+3. Teacher must not have more than 4 classes in a day
+4. Teacher must have a day off
+5. Teacher must not have multiple lectures at the same time
 */
 class Teacher{
-	
-	/*
-	these methods are used when creating teacher objects from json.
-	The teacher must have a default constructor for this purpose
-	*/
-	friend void to_json(json& , const Teacher&);
-	friend void from_json(const json&, Teacher&);
 
-	public:
+    //creates C++ Teacher object from json Teacher object
+    friend void from_json(const nlohmann::json&, Teacher&);
 
-		~Teacher(){
-			//this only removes the pointers
-			//from vector without deleting
-			lectures.clear();
-		}
+    public:
 
-		//this will be called when creating lecture entries
-		void addLecture(Lecture* lecture){
-			lectures.push_back(lecture);
-		}
-		
-		const string& getId() const {return id;}
-		
-		const string& getName() const {return name;}
-		
-		const string& getDepartment() const {return department;}
+        const std::string& getId() const {return id;}
 
-		const vector<Lecture*>& getLectures() const {return lectures;}
+        const std::string& getDepartment() const {return department;}
 
-		bool getAvailableSlot(int position) const {return availableSlots[position];}
+        const std::vector<std::string>& getLectures() const {return lectures;}
 
-	private:
+        const AVAILABOOL& getAvailableSlots() const {return availableSlots;}
 
-		string	id;
-		string	name;
-		string	department;
+        const std::vector<std::string>& getAvailableRooms() const {return availableRooms;}
 
-		//lectures assigned to this teacher
-		vector<Lecture*> lectures;
+        void addLecture(std::string lecture){lectures.push_back(lecture);}
 
-		vector<bool> availableSlots;
+    private:
+
+        std::string id;
+        std::string department;
+
+        std::vector<std::string> lectures;
+
+        AVAILABOOL availableSlots;
+        std::vector<std::string> availableRooms;
+
 };
 
-void to_json(json& j, const Teacher& teacher) {
-	j = json{
-		{"id", teacher.id},
-		{"name", teacher.name},
-		{"department", teacher.department},
-		{"availableSlots", teacher.availableSlots}
-	};
-}
+void from_json(const nlohmann::json& jteacher, Teacher& teacher) {
+	jteacher.at("id").get_to(teacher.id);
+	jteacher.at("department").get_to(teacher.department);
+	jteacher.at("availableSlots").get_to(teacher.availableSlots);
+	jteacher.at("availableRooms").get_to(teacher.availableRooms);
 
-void from_json(const json& j, Teacher& teacher) {
-	j.at("id").get_to(teacher.id);
-	j.at("name").get_to(teacher.name);
-	j.at("department").get_to(teacher.department);
-	j.at("availableSlots").get_to(teacher.availableSlots);
+    std::sort(teacher.availableRooms.begin(), teacher.availableRooms.end());
 }
 
 #endif
